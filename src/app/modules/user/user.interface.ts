efdc3ce1940cea20dd9c4d56c4239d89ec6e50aa
tanjiina <tanjina1042@gmail.com>
2025-10-24 21:32:47 +0600

    user model and validation using zod create

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,8 @@
-export type user = {
+export type TUser = {
   id: string;
   password: string;
   needsPasswordChange: boolean;
   role: "admin" | "student" | "faculty";
+  status: "in-progress" | "blocked";
   isDeleted: boolean;
 };
