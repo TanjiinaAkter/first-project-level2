7cf2e29e525e33090c0fb4267dd4bb5298242d31
tanjiina <tanjina1042@gmail.com>
2025-10-27 02:09:50 +0600

    create index route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
+// import { StudentRoutes } from "./app/modules/student/student.route";
+// import { UserRoutes } from "./app/modules/user/user.route";
 import { error } from "console";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
 
 const app: Application = express();
 // const port = 3000;
@@ -15,13 +16,14 @@ app.use(express.json());
 app.use(cors());
 
 //APPLICATION ROUTES
-app.use("/api/v1/students", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
-const getAController = (req: Request, res: Response) => {
+app.use("/api/v1", router);
+// app.use("/api/v1/students", StudentRoutes);
+// app.use("/api/v1/users", UserRoutes);
+const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
-app.get("/", getAController);
+app.get("/", test);
 //global error handler
 app.use(globalErrorHandler);
 // not found
